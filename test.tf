bb474ff7675f532f153a943f0453064b02041747
Lagorce <vlagorce@gmail.com>
2022-06-02 16:41:23 +0200

    Update test.tf

--- test.tf
+++ test.tf
@@ -6,7 +6,7 @@ resource "kubernetes_cron_job" "kaniko_project_executor" {
           spec {
             container {
               name    = "kaniko"
-              image   = "gcr.io/kaniko-project/executor:latest"
+              image   = "gcr.io/kaniko-project/executor:v1.7.0@sha256:8504bde9a9a8c9c4e9a4fe659703d265697a36ff13607b7669a4caa4407baa52"
             }
           }
         }
