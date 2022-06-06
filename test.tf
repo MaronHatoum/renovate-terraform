57969f2c00f8dbcfb8df3982358954bd841c1946
Maron <98313426+MaronHatoum@users.noreply.github.com>
2022-06-06 16:43:28 +0300

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,12 +1,13 @@
-resource "kubernetes_cron_job" "kaniko_project_executor" {
+resource "kubernetes_cron_job" "foo" {
   spec {
     job_template {
       spec {
+        ...
         template {
           spec {
             container {
-              name    = "kaniko"
-              image   = "gcr.io/kaniko-project/executor:v1.7.0@sha256:8504bde9a9a8c9c4e9a4fe659703d265697a36ff13607b7669a4caa4407baa52"
+              name    = "my-image"
+              image   = "my.registry.io/my-repo/my-image:1.0.0@sha256:123456" 
             }
           }
         }
